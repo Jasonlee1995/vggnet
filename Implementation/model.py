fdeecc9f11f1707c8100d4ad2e244d1ee01f8ef3
Jason <2014145110@yonsei.ac.kr>
2020-07-11 21:59:57 +0900

    Fixed minor changes on model.py
    add inplace on relu

--- Implementation/model.py
+++ Implementation/model.py
@@ -36,8 +36,8 @@ class VGG(nn.Module):
         self.features = features
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096), nn.ReLU(True), nn.Dropout(),
-            nn.Linear(4096, 4096), nn.ReLU(True), nn.Dropout(),
+            nn.Linear(512 * 7 * 7, 4096), nn.ReLU(inplace=True), nn.Dropout(),
+            nn.Linear(4096, 4096), nn.ReLU(inplace=True), nn.Dropout(),
             nn.Linear(4096, num_classes)
         )
         if init_weights:
